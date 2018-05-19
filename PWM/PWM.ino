f6c6d73130f7b26216a84ba52b6cc64fce8de1b5
alfredlima <alfredolms@ic.ufal.br>
2018-05-19 16:07:38 -0300

    PWM

--- PWM/PWM.ino
+++ PWM/PWM.ino
@@ -3,7 +3,7 @@ int LEDpin = D5;
 
 void setup(){
   Serial.begin(115200);
-  analogWrite(LEDpin, 512+256);  /* set initial 50% duty cycle */
+  analogWrite(LEDpin, 512);  /* set initial 50% duty cycle */
 }
 
 void loop(){
